293213546bbcd83a94321d543a02361561b90b60
Alvaro Galisteo <alvarogalisteo@outlook.es>
2019-08-07 00:28:35 +0200

    Renamed images and added database

---
+++ db.js
@@ -0,0 +1,12 @@
+var db = {
+  "drawing": {
+    "sketchbook1819": [1,39],
+    "tradicional": [40,47],
+    "digital": [48,53]
+  },
+  "photography": {
+  	"bw": null,
+  	"color": null,
+  	"rem": null
+  }
+}
