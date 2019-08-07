7e1180df42c06a8e1a4961cb10caeaadf20a940e
Alvaro Galisteo <alvarogalisteo@outlook.es>
2019-08-07 23:54:09 +0200

    Added image navigation wireframe and image lazy load

--- display.js
+++ display.js
@@ -3,41 +3,42 @@ function show(category) {
 	var content = "";
 	if(range !== null){
 		for (var i = range[0]; i <= range[1]; i++) {
-			content += '<img src="files/portfolio/IMG_' + i + '.png">'
+			content += '<a href="view.html?img=' + i + '"><img class="lazyload" src=data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw== data-src="files/portfolio/IMG_' + i + '.png"></a>'
 		}
 	}
 	document.getElementById('image').innerHTML = content;
 	document.getElementById(current).classList.remove('active');
 	current = category;
 	document.getElementById(current).classList.add('active');
+	lazyload();
 }
 
 function image() {
     var url = new URL(window.location.href);
-    var img = url.searchParams.get("img");
+    var img = parseInt(url.searchParams.get("img"));
     document.getElementById("view-image").src = "files/portfolio/IMG_" + img + ".png";
 }
 
 function nextImg() {
     var url = new URL(window.location.href);
-    var img = url.searchParams.get("img");
-    if(img+1 <= db[ranges][1]){
-        window.history.pushState("","","/view.html?img=" + (img+1));
+    var img = parseInt(url.searchParams.get("img"));
+    if(img+1 <= db.ranges[1]){
+        window.history.pushState("","","view.html?img=" + (img+1));
         image();
     }
 }
 
 function prevImg() {
     var url = new URL(window.location.href);
-    var img = url.searchParams.get("img");
-    if(img-1 >= db[ranges][0]){
-        window.history.pushState("","","/view.html?img=" + (img-1));
+    var img = parseInt(url.searchParams.get("img"));
+    if(img-1 >= db.ranges[0]){
+        window.history.pushState("","","view.html?img=" + (img-1));
         image();
     }
 }
 
-console.log("Type: " + type + ", category: " + current);
 if (!window.location.pathname.includes("view.html")) {
+	console.log("Type: " + type + ", category: " + current);
     show(current);	
 } else {
     image();
