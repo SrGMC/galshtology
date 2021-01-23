d828e2f39891b6ff0fcb89c470a1a592b0df2cda
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2021-01-23 13:29:06 +0100

    Formatting and analytics

--- db.js
+++ db.js
@@ -1,40 +1,43 @@
 var db = {
-  "drawing": {
-    "sketchbook1819": {
-        "count": 39,
-        "subtitle": ""
+    drawing: {
+        sketchbook1819: {
+            count: 39,
+            subtitle: "",
+        },
+        tradicional: {
+            count: 8,
+            subtitle: "",
+        },
+        digital: {
+            count: 10,
+            subtitle: "",
+        },
     },
-    "tradicional": {
-        "count": 8,
-        "subtitle": ""
+    design: {
+        eco2: {
+            count: 2,
+            subtitle:
+                '<h1>Zapatilla ECO-2</h1><br><h2>Diseño</h2><br>Como decía Louis Sullivan "La forma sigue a la función" y este diseño lo refleja. La función de nuestro producto debe ser la sujeción correcta del pie, especialmente del tobillo, y ello lo vemos en la forma que este tiene: múltiples cintas que rodean zonas específicas del pie. Además el diseño cuenta con una suela dividida, que dota a la zapatilla flexibilidad para su mejor movimiento, y se ve complementada de una mediasuela para no dejar la zona al descubierto.<br><br>La imagen del producto tiene un aire industrial, urbano, por lo que se utilizan colores y texturas presentes en la ciudad. Las texturas rugosas se asemejan al pavimento y asfalto de la urbe, y las texturas lisas a los vidrios y metales de los edificios. En la paleta de color predominan los grises, pero también encontramos colores más vibrantes como el naranja (color predominante en las protecciones de obra) y el azul, el cielo que a veces se atisba entre los edificios.<br><br><h2>Packaging</h2><br>También he realizado una propuesta de packaging, pues toda zapatilla necesita su caja.<br><br>Siguiendo la línea ecológica del diseño, este embalaje se hará de cartón reciclado. Su estética exterior es también industrial y sus formas son geométricas.<br><br>La caja se forma de una sola pieza, que será cortada por un troquel y posteriormente impresa en su exterior con tres flechas, asemejándose al embalaje industrial. Una vez montada, su silueta forma un triángulo, además se añadirá un asa con grapas en la parte lateral izquierda.<br><br>La colocación de las zapatillas será diferente a lo común, se colocarán con ambas suelas enfrentadas. Esto da una estética moderna al producto.',
+        },
     },
-    "digital": {
-        "count": 10,
-        "subtitle": ""
-    }
-  },
-  "design": {
-    "eco2": {
-        "count": 2,
-        "subtitle": "<h1>Zapatilla ECO-2</h1><br><h2>Diseño</h2><br>Como decía Louis Sullivan \"La forma sigue a la función\" y este diseño lo refleja. La función de nuestro producto debe ser la sujeción correcta del pie, especialmente del tobillo, y ello lo vemos en la forma que este tiene: múltiples cintas que rodean zonas específicas del pie. Además el diseño cuenta con una suela dividida, que dota a la zapatilla flexibilidad para su mejor movimiento, y se ve complementada de una mediasuela para no dejar la zona al descubierto.<br><br>La imagen del producto tiene un aire industrial, urbano, por lo que se utilizan colores y texturas presentes en la ciudad. Las texturas rugosas se asemejan al pavimento y asfalto de la urbe, y las texturas lisas a los vidrios y metales de los edificios. En la paleta de color predominan los grises, pero también encontramos colores más vibrantes como el naranja (color predominante en las protecciones de obra) y el azul, el cielo que a veces se atisba entre los edificios.<br><br><h2>Packaging</h2><br>También he realizado una propuesta de packaging, pues toda zapatilla necesita su caja.<br><br>Siguiendo la línea ecológica del diseño, este embalaje se hará de cartón reciclado. Su estética exterior es también industrial y sus formas son geométricas.<br><br>La caja se forma de una sola pieza, que será cortada por un troquel y posteriormente impresa en su exterior con tres flechas, asemejándose al embalaje industrial. Una vez montada, su silueta forma un triángulo, además se añadirá un asa con grapas en la parte lateral izquierda.<br><br>La colocación de las zapatillas será diferente a lo común, se colocarán con ambas suelas enfrentadas. Esto da una estética moderna al producto."
-    }
-  },
-  "photography": {
-  	"bw": {
-        "count": 9,
-        "subtitle": ""
+    photography: {
+        bw: {
+            count: 9,
+            subtitle: "",
+        },
+        color: {
+            count: 23,
+            subtitle: "",
+        },
+        rem: {
+            count: 7,
+            subtitle:
+                '<h1>REM: Los 7 Pecados Capitales</h1><br>El título que he dado a la obra es la palabra "realidad" en latín, al igual que el nombre de cada imagen, ya que consideré que complementaba muy bien la temática. En este proyecto he querido representar los famosos pecados capitales, siendo estos: lujuria, pereza, soberbia, gula, ira, envidia y avaricia.<br><br>El objetivo era dar una visión diferente a la habitual de estas emociones. Ha sido un reto, pues es muy complicado representar emociones humanas en imágenes. Al ser metamorfosis la temática elegida he decidido aplicar un elemento común entre la mayoría de las imágenes, siendo este un barrido en diferentes campos de vista. <br><br>Previo a la realización se ha preparado un brief para intentar visualizar el producto final.',
+        },
+        ide: {
+            count: 5,
+            subtitle:
+                "<h1>La intimidad del ente</h1><br>El contacto es la base de la existencia humana, por lo que he querido rendirle homenaje. A lo largo del día es fácil percibir un amplio abanico de relaciones interpersonales, por lo que me pregunté, ¿cómo puedo representar un acto propiamente humano sin este estar presente? La clave está en la historia del arte, comprender como se expresa el ser humano y llevarlo al ámbito fotográfico.<br><br>Para ello he seleccionado 5 tipos: pasional/carnal; familiar/de grupo; expectativa; religioso;agresivo. Las imágenes serán presentadas en parejas, ya que para haber contacto es necesario un plural.<br><br>La obra estará marcada por un estilo suprematista y un estudio de la forma, tomando inspiración de Malevich, Rodchenko y Mapplethorpe. Este último, con su fotografía floral es quien motiva la realización de la obra.<br><br>De esta manera nos planteamos la pregunta ¿como puedo lograr más con menos?",
+        },
     },
-  	"color": {
-        "count": 23,
-        "subtitle": ""
-    },
-  	"rem": {
-        "count": 7,
-        "subtitle": "<h1>REM: Los 7 Pecados Capitales</h1><br>El título que he dado a la obra es la palabra \"realidad\" en latín, al igual que el nombre de cada imagen, ya que consideré que complementaba muy bien la temática. En este proyecto he querido representar los famosos pecados capitales, siendo estos: lujuria, pereza, soberbia, gula, ira, envidia y avaricia.<br><br>El objetivo era dar una visión diferente a la habitual de estas emociones. Ha sido un reto, pues es muy complicado representar emociones humanas en imágenes. Al ser metamorfosis la temática elegida he decidido aplicar un elemento común entre la mayoría de las imágenes, siendo este un barrido en diferentes campos de vista. <br><br>Previo a la realización se ha preparado un brief para intentar visualizar el producto final."
-    },
-  	"ide":{
-        "count": 5,
-        "subtitle": "<h1>La intimidad del ente</h1><br>El contacto es la base de la existencia humana, por lo que he querido rendirle homenaje. A lo largo del día es fácil percibir un amplio abanico de relaciones interpersonales, por lo que me pregunté, ¿cómo puedo representar un acto propiamente humano sin este estar presente? La clave está en la historia del arte, comprender como se expresa el ser humano y llevarlo al ámbito fotográfico.<br><br>Para ello he seleccionado 5 tipos: pasional/carnal; familiar/de grupo; expectativa; religioso;agresivo. Las imágenes serán presentadas en parejas, ya que para haber contacto es necesario un plural.<br><br>La obra estará marcada por un estilo suprematista y un estudio de la forma, tomando inspiración de Malevich, Rodchenko y Mapplethorpe. Este último, con su fotografía floral es quien motiva la realización de la obra.<br><br>De esta manera nos planteamos la pregunta ¿como puedo lograr más con menos?"
-    }
-  }
-}
+};
--- display.js
+++ display.js
@@ -1,17 +1,30 @@
 function show(category) {
-	var range = db[type][category].count;
-	document.querySelector('#subtitle').innerHTML = db[type][category].subtitle;
-	var content = "";
-	if(range !== null){
-		for (var i = 1; i <= range; i++) {
-			content += '<a href="view.html?img=' + i + '&type=' + type + '&cat=' + category + '"><img class="lazyload" src=data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw== data-src="files/portfolio/' + category + '/' + category + '_' + i + '.png"></a>'
-		}
-	}
-	document.getElementById('image').innerHTML = content;
-	document.getElementById(current).classList.remove('active');
-	current = category;
-	document.getElementById(current).classList.add('active');
-	lazyload();
+    var range = db[type][category].count;
+    document.querySelector("#subtitle").innerHTML = db[type][category].subtitle;
+    var content = "";
+    if (range !== null) {
+        for (var i = 1; i <= range; i++) {
+            content +=
+                '<a href="view.html?img=' +
+                i +
+                "&type=" +
+                type +
+                "&cat=" +
+                category +
+                '"><img class="lazyload" src=data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw== data-src="files/portfolio/' +
+                category +
+                "/" +
+                category +
+                "_" +
+                i +
+                '.png"></a>';
+        }
+    }
+    document.getElementById("image").innerHTML = content;
+    document.getElementById(current).classList.remove("active");
+    current = category;
+    document.getElementById(current).classList.add("active");
+    lazyload();
 }
 
 function image() {
@@ -21,16 +34,16 @@ function image() {
     var type = url.searchParams.get("type");
     document.getElementById("view-image").src = "files/portfolio/" + cat + "/" + cat + "_" + img + ".png";
 
-    if(img+1 <= db[type][cat].count){
-    	document.getElementById("nav_next").classList.remove('hidden');
+    if (img + 1 <= db[type][cat].count) {
+        document.getElementById("nav_next").classList.remove("hidden");
     } else {
-    	document.getElementById("nav_next").classList.add('hidden');
+        document.getElementById("nav_next").classList.add("hidden");
     }
 
-    if(img-1 >= 1){
-    	document.getElementById("nav_prev").classList.remove('hidden');
+    if (img - 1 >= 1) {
+        document.getElementById("nav_prev").classList.remove("hidden");
     } else {
-    	document.getElementById("nav_prev").classList.add('hidden');
+        document.getElementById("nav_prev").classList.add("hidden");
     }
 }
 
@@ -39,8 +52,8 @@ function nextImg() {
     var img = parseInt(url.searchParams.get("img"));
     var cat = url.searchParams.get("cat");
     var type = url.searchParams.get("type");
-    if(img+1 <= db[type][cat].count){
-        window.history.replaceState("","Galshtology","view.html?img=" + (img+1) + '&type=' + type + '&cat=' + cat);
+    if (img + 1 <= db[type][cat].count) {
+        window.history.replaceState("", "Galshtology", "view.html?img=" + (img + 1) + "&type=" + type + "&cat=" + cat);
         image();
     }
 }
@@ -50,15 +63,15 @@ function prevImg() {
     var img = parseInt(url.searchParams.get("img"));
     var cat = url.searchParams.get("cat");
     var type = url.searchParams.get("type");
-    if(img-1 >= 1){
-        window.history.replaceState("","Galshtology","view.html?img=" + (img-1) + '&type=' + type + '&cat=' + cat);
+    if (img - 1 >= 1) {
+        window.history.replaceState("", "Galshtology", "view.html?img=" + (img - 1) + "&type=" + type + "&cat=" + cat);
         image();
     }
 }
 
 if (!window.location.pathname.includes("view.html")) {
-	console.log("Type: " + type + ", category: " + current);
-    show(current);	
+    console.log("Type: " + type + ", category: " + current);
+    show(current);
 } else {
     image();
 }
